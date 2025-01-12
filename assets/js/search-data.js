68f6011706d8effc536b69ad46fd0ac3ffccdee7
peifferjd <55062693+peifferjd@users.noreply.github.com>
2025-01-12 23:09:27 +0000

    Deploying to gh-pages from @ peifferjd/peifferjd.github.io@4a8cae2fa59db7bdc2c411547cf000280615db10 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%70%65%69%66%66%65%72@%73%72%61%6C%61%62.%6F%72%67", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/peiffer_jd", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -500,6 +507,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,7 +527,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=JuoR3ewAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
