206aff90616699f6615ff979226e611106cf04c5
peifferjd <55062693+peifferjd@users.noreply.github.com>
2025-01-13 00:01:40 +0000

    Deploying to gh-pages from @ peifferjd/peifferjd.github.io@e5caa4695c60671a86ab8384419ac909ea13c63e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -502,6 +502,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/jdpeiffer", "_blank");
         },
       },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
